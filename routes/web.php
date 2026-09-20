52ca6ad50ddf126db80aa32fdf425a63efcd056b
Waks Kenneth <42246236+Engwaksken@users.noreply.github.com>
2026-09-20 20:02:36 +0300

    Enable media update route

--- routes/web.php
+++ routes/web.php
@@ -43,89 +43,55 @@ Route::middleware(['auth', 'cms.access'])
 
         Route::resource('organisation-units', OrganisationUnitController::class)
             ->except(['show', 'create', 'edit']);
-
         Route::resource('content', ContentController::class)
             ->except(['show', 'create', 'edit']);
-
         Route::resource('events', EventController::class)
             ->except(['show', 'create', 'edit']);
-
         Route::resource('life-groups', LifeGroupController::class)
             ->except(['show', 'create', 'edit']);
-
         Route::resource('courses', CourseController::class)
             ->except(['show', 'create', 'edit']);
 
-        Route::post('courses/{course}/lessons', [CourseController::class, 'storeLesson'])
-            ->name('courses.lessons.store');
-        Route::delete('courses/{course}/lessons/{lesson}', [CourseController::class, 'destroyLesson'])
-            ->name('courses.lessons.destroy');
-
-        Route::get('courses/{course}/quizzes', [QuizController::class, 'index'])
-            ->name('courses.quizzes.index');
-        Route::post('courses/{course}/quizzes', [QuizController::class, 'store'])
-            ->name('courses.quizzes.store');
-        Route::post('quizzes/{quiz}/questions', [QuizController::class, 'storeQuestion'])
-            ->name('quizzes.questions.store');
-        Route::delete('quizzes/{quiz}', [QuizController::class, 'destroy'])
-            ->name('quizzes.destroy');
-        Route::delete('quizzes/{quiz}/questions/{question}', [QuizController::class, 'destroyQuestion'])
-            ->name('quizzes.questions.destroy');
+        Route::post('courses/{course}/lessons', [CourseController::class, 'storeLesson'])->name('courses.lessons.store');
+        Route::delete('courses/{course}/lessons/{lesson}', [CourseController::class, 'destroyLesson'])->name('courses.lessons.destroy');
+        Route::get('courses/{course}/quizzes', [QuizController::class, 'index'])->name('courses.quizzes.index');
+        Route::post('courses/{course}/quizzes', [QuizController::class, 'store'])->name('courses.quizzes.store');
+        Route::post('quizzes/{quiz}/questions', [QuizController::class, 'storeQuestion'])->name('quizzes.questions.store');
+        Route::delete('quizzes/{quiz}', [QuizController::class, 'destroy'])->name('quizzes.destroy');
+        Route::delete('quizzes/{quiz}/questions/{question}', [QuizController::class, 'destroyQuestion'])->name('quizzes.questions.destroy');
 
         Route::resource('media', MediaAssetController::class)
-            ->only(['index', 'store', 'destroy']);
+            ->only(['index', 'store', 'update', 'destroy']);
 
         Route::resource('church-locations', ChurchLocationController::class)
             ->except(['show', 'create', 'edit']);
-
         Route::resource('payment-gateways', PaymentGatewayController::class)
             ->except(['show', 'create', 'edit']);
 
-        Route::get('donations', [DonationController::class, 'index'])
-            ->name('donations.index');
-        Route::post('donations/campaigns', [DonationController::class, 'storeCampaign'])
-            ->name('donations.campaigns.store');
-        Route::put('donations/campaigns/{campaign}', [DonationController::class, 'updateCampaign'])
-            ->name('donations.campaigns.update');
-        Route::delete('donations/campaigns/{campaign}', [DonationController::class, 'destroyCampaign'])
-            ->name('donations.campaigns.destroy');
+        Route::get('donations', [DonationController::class, 'index'])->name('donations.index');
+        Route::post('donations/campaigns', [DonationController::class, 'storeCampaign'])->name('donations.campaigns.store');
+        Route::put('donations/campaigns/{campaign}', [DonationController::class, 'updateCampaign'])->name('donations.campaigns.update');
+        Route::delete('donations/campaigns/{campaign}', [DonationController::class, 'destroyCampaign'])->name('donations.campaigns.destroy');
 
-        Route::get('ai', [AiSettingController::class, 'index'])
-            ->name('ai.index');
-        Route::put('ai', [AiSettingController::class, 'update'])
-            ->name('ai.update');
-        Route::post('ai/test', [AiSettingController::class, 'test'])
-            ->name('ai.test');
+        Route::get('ai', [AiSettingController::class, 'index'])->name('ai.index');
+        Route::put('ai', [AiSettingController::class, 'update'])->name('ai.update');
+        Route::post('ai/test', [AiSettingController::class, 'test'])->name('ai.test');
 
-        Route::get('prayer', [PrayerRequestController::class, 'index'])
-            ->name('prayer.index');
-        Route::put('prayer/{prayerRequest}', [PrayerRequestController::class, 'update'])
-            ->name('prayer.update');
+        Route::get('prayer', [PrayerRequestController::class, 'index'])->name('prayer.index');
+        Route::put('prayer/{prayerRequest}', [PrayerRequestController::class, 'update'])->name('prayer.update');
 
-        Route::get('moderation', [ModerationController::class, 'index'])
-            ->name('moderation.index');
-        Route::put('moderation/{report}', [ModerationController::class, 'resolve'])
-            ->name('moderation.resolve');
+        Route::get('moderation', [ModerationController::class, 'index'])->name('moderation.index');
+        Route::put('moderation/{report}', [ModerationController::class, 'resolve'])->name('moderation.resolve');
 
-        Route::get('reports', [ReportController::class, 'index'])
-            ->name('reports.index');
+        Route::get('reports', [ReportController::class, 'index'])->name('reports.index');
+        Route::get('system/health', [SystemHealthController::class, 'index'])->name('system.health');
 
-        Route::get('system/health', [SystemHealthController::class, 'index'])
-            ->name('system.health');
+        Route::get('guardian-consents', [GuardianConsentController::class, 'index'])->name('guardian-consents.index');
+        Route::post('guardian-consents/{guardianConsent}/approve', [GuardianConsentController::class, 'approve'])->name('guardian-consents.approve');
+        Route::post('guardian-consents/{guardianConsent}/reject', [GuardianConsentController::class, 'reject'])->name('guardian-consents.reject');
 
-        Route::get('guardian-consents', [GuardianConsentController::class, 'index'])
-            ->name('guardian-consents.index');
-        Route::post('guardian-consents/{guardianConsent}/approve', [GuardianConsentController::class, 'approve'])
-            ->name('guardian-consents.approve');
-        Route::post('guardian-consents/{guardianConsent}/reject', [GuardianConsentController::class, 'reject'])
-            ->name('guardian-consents.reject');
-
-        Route::get('notifications', [NotificationController::class, 'index'])
-            ->name('notifications.index');
-        Route::post('notifications', [NotificationController::class, 'store'])
-            ->name('notifications.store');
-        Route::put('notifications/{notification}', [NotificationController::class, 'update'])
-            ->name('notifications.update');
-        Route::delete('notifications/{notification}', [NotificationController::class, 'destroy'])
-            ->name('notifications.destroy');
+        Route::get('notifications', [NotificationController::class, 'index'])->name('notifications.index');
+        Route::post('notifications', [NotificationController::class, 'store'])->name('notifications.store');
+        Route::put('notifications/{notification}', [NotificationController::class, 'update'])->name('notifications.update');
+        Route::delete('notifications/{notification}', [NotificationController::class, 'destroy'])->name('notifications.destroy');
     });
