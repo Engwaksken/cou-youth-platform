6ff9d76decbbf7f2fccc616c4ebfd025a6090d8f
Waks Kenneth <42246236+Engwaksken@users.noreply.github.com>
2026-09-25 19:19:23 +0300

    Load youth engagement web routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,26 +7,23 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         $this->configureRateLimiters();
+
+        Route::middleware('web')->group(base_path('routes/youth-engagement.php'));
 
         View::composer('*', function ($view): void {
             try {
