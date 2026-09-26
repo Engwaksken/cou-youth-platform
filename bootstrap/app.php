3d3e2c994c17b0a2aba199165d70969d40dee30e
Waks Kenneth <42246236+Engwaksken@users.noreply.github.com>
2026-09-26 12:24:28 +0300

    Enable central CMS mutation audit logging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AuditCmsMutations;
 use App\Http\Middleware\EnsureCmsAccess;
 use App\Http\Middleware\EnsureCmsModuleAccess;
 use App\Http\Middleware\SecurityHeaders;
@@ -20,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append(SecurityHeaders::class);
+        $middleware->append(AuditCmsMutations::class);
 
         $middleware->alias([
             'cms.access' => EnsureCmsAccess::class,
